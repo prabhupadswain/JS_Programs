cb7d8623b394ad02be754c4330ff185b09862046
prabhupadswain <prabhupad.swain@outlook.com>
2021-12-23 18:43:25 +0530

    Rest Parameter using Rest Operator(...)

--- Rest Operator in JS/demo-rest_parameter.js
+++ Rest Operator in JS/demo-rest_parameter.js
@@ -1,5 +1,6 @@
-
-//Rest operator = args
+//Rest operator = ...
+//...args is the rest paramter here in sum function
+//Calculate sum of the list of numbers as an argument send to a function.
 const sum = (... args) => {
 	let total = 0;
 	for(const a of args) {
@@ -8,4 +9,21 @@ const sum = (... args) => {
 	return total;
 }
 
-console.log(sum(10,20,30));
+console.log(sum(10,20,30)); //Prints the value of total return from sum arrow function.
+//Output : 60
+
+//-----------------------------------------------------------------------------------------------------
+
+//Another demonstration to multiply the list of numbers sent as an argument to a function.
+//Rest parameter = productArgs
+const product = (...productArgs) => {
+	return productArgs.reduce((previous, current) => {
+	  return previous * current;
+	});
+  }
+  
+  console.log(product(1, 2, 3));
+  //Output : 6
+  
+  console.log(product(1, 2, 3, 4));
+  //Output : 24
